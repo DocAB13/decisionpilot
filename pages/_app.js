33bee68c4a1d9a75e7d38bd4b34d5aaa98ad77ec
Nicolae-Abel Bolos-Ardeleanu <bolos_abel@yahoo.com>
2026-06-21 00:02:41 +0200

    Add PWA support with icons and manifest

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Head from 'next/head'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import CookieBanner from '../components/CookieBanner'
 
@@ -7,6 +8,15 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" href="/icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="DecisionPilot" />
+      </Head>
       <Component {...pageProps} />
       <CookieBanner
         onAccept={() => setAnalyticsEnabled(true)}
