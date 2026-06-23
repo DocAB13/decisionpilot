416bd626ef925baf7c7e9e5863743b2ef707fed5
DocAB13 <bolos_abel@yahoo.com>
2026-06-23 20:08:08 +0200

    Update impact site verification meta tag

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@600;700;800;900&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <<meta name='impact-site-verification' value='2317ff5d-362b-4903-a7c0-92a03ee7e9e8'>
+<meta name='impact-site-verification' value='e40c1e40-479a-42f7-8dc4-bfef1c8d6a32'>
 </Head>
       <App />
       <div style={{ textAlign: 'center', padding: '24px', background: '#F8F9FC' }}>
